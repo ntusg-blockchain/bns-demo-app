5217718fdd372a0a4f5f4057f027cd6961a6e9a6
BernardChoo <bernardchoowk@gmail.com>
2020-10-28 10:45:37 +0800

    add in poweredby header.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const webpack = require('webpack');
 const { createSecureHeaders } = require('next-secure-headers');
 
 module.exports = {
+	//In general, X-Powered-By HTTP response header should be removed from response headers because it helps hackers to get the server information.
+	poweredByHeader: false,
+
 	async headers() {
 		return [
 			{
