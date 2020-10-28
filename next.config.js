df659bd4657a407866add8d5ee55638fad7e07b9
BernardChoo <bernardchoowk@gmail.com>
2020-10-28 10:09:21 +0800

    add in default secure headers.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
-
 //Set up dotenv
-const { parsed: localEnv } = require('dotenv').config()
-const webpack = require('webpack')
+const { parsed: localEnv } = require('dotenv').config();
+const webpack = require('webpack');
+const { createSecureHeaders } = require('next-secure-headers');
 
 module.exports = {
-  webpack: (config, { buildId, dev }) => {
-    // This allows the app to refer to files through our symlink
-    config.resolve.symlinks = false
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv)) //dotenv
-    return config
-  }
-}
+	async headers() {
+		return [ { source: '/(.*)', headers: createSecureHeaders() } ];
+	},
+	webpack: (config, { buildId, dev }) => {
+		// This allows the app to refer to files through our symlink
+		config.resolve.symlinks = false;
+		config.plugins.push(new webpack.EnvironmentPlugin(localEnv)); //dotenv
+		return config;
+	}
+};
