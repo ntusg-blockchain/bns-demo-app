83621bbb30b47cc0c4cbab774fa06c54f82dc281
BernardChoo <bernardchoowk@gmail.com>
2021-05-15 23:06:01 +0800

    revert static setup.

--- next.config.js
+++ next.config.js
@@ -1,48 +1,11 @@
 //Set up dotenv
 const { parsed: localEnv } = require('dotenv').config();
 const webpack = require('webpack');
-// const { createSecureHeaders } = require('next-secure-headers');
-const debug = process.env.NODE_ENV !== "production";
+const { createSecureHeaders } = require('next-secure-headers');
 
 module.exports = {
-	// Githug page.
-	exportPathMap: function () {
-		return {
-		  "/": { page: "/" },
-		  "/about": { page: "/about" },
-		  "/attendanceTracker": { page: "/attendanceTracker" },
-		  "/integerstorage": { page: "/integerstorage" },
-		  "/quickguide": { page: "/quickguide" },
-		  "/callIntegerStorage": { page: "/callIntegerStorage" },
-		  "/signMessage": { page: "/signMessage" },
-		  "/404": { page: "/404" }
-		}
-	  },
-	  //assetPrefix: '',
-	  assetPrefix: !debug ? '/bns-demo-app/blob/gh-pages/' : '',
-	  webpack: (config, { dev }) => {
-		// Perform customizations to webpack config
-		// console.log('webpack');
-		// console.log(config.module.rules, dev);
-		config.module.rules = config.module.rules.map(rule => {
-		  if(rule.loader === 'babel-loader') {
-			rule.options.cacheDirectory = false
-		  }
-		  return rule
-		})
-		// Important: return the modified config
-		return config
-	  },/*,
-	  webpackDevMiddleware: (config) => {
-		// Perform customizations to webpack dev middleware config
-		// console.log('webpackDevMiddleware');
-		// console.log(config);
-		// Important: return the modified config
-		return config
-	  }, */
-
 	//In general, X-Powered-By HTTP response header should be removed from response headers because it helps hackers to get the server information.
-	/*poweredByHeader: false,
+	poweredByHeader: false,
 
 	async headers() {
 		return [
@@ -58,5 +21,4 @@ module.exports = {
 		config.plugins.push(new webpack.EnvironmentPlugin(localEnv)); //dotenv
 		return config;
 	}
-	*/
 };
